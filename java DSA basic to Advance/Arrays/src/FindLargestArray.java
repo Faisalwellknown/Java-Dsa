7725d5bb6562ab092fb03e96561cf9afad94f351
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-07-14 04:21:02 +0530

    Added find largest element in an array

---
+++ java DSA basic to Advance/Arrays/src/FindLargestArray.java
@@ -0,0 +1,20 @@
+ import java.util.*;
+public class FindLargestArray {
+    
+
+    public static void main(String [] args){
+        int[] num = {5,3,4,2,1,} ;
+        Arrays.sort(num);
+
+        int largest =  num[num.length -1];
+
+        System.out.println("Largest number in array: " + largest);
+
+
+
+        }
+    }
+
+
+
+
