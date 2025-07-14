295beba2474a43192483063e966045bef51df899
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-07-14 07:02:10 +0530

    Added optimal largest element in array

---
+++ java DSA basic to Advance/Arrays/src/OptimalArrayLargest.java
@@ -0,0 +1,18 @@
+public class OptimalArrayLargest {
+    
+
+    public static void main(String[] args) {
+        int[] array = {4,2,3,8,1};
+        int asumeelement = array[0];
+        for(int i = 1;i < array.length;i++){
+            if (array[i] > asumeelement  ){
+                asumeelement = array[i];
+            }
+        }
+        System.out.println(asumeelement);
+    }
+
+}
+
+
+
