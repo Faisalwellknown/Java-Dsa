785ce545f671285e6792a847aac242bad70e773d
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-07-16 04:58:44 +0530

    Added 2D Array Digonal Sum

---
+++ java DSA basic to Advance/Arrays/src/Array2DDigonalsum.java
@@ -0,0 +1,44 @@
+public class Array2DDigonalsum {
+   
+
+    public static int digosum(int[][] mat){
+        //check if matrix is emty
+        if(mat == null || mat.length == 0 || mat[0].length == 0){
+            return 0;
+
+
+        }
+        int size = mat.length;
+        System.out.println(size);
+        int total = 0;
+
+        for(int i = 0;i < size;i++){
+            total = total + mat[i][i];
+            total = total + mat[i][size -1 - i];
+        }
+
+        if(size % 2 ==1){
+            total = total - mat[size/2][size/2];
+        }
+
+        return total;
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[][] array = {{1,2,3},
+                         {4,5,6},
+                         {7,8,9}};
+
+        int result = digosum(array);
+        System.out.println(result);
+
+    }
+}
+
+
+
