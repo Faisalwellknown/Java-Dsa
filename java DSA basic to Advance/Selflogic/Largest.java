9080b8618fee9af2c180bfd6868ae19335a57a3d
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-07-26 07:11:22 +0530

    Added selflogilc folder and Largest and loop file

---
+++ java DSA basic to Advance/Selflogic/Largest.java
@@ -0,0 +1,24 @@
+
+
+public class Largest {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 20;
+        int c= 30;
+        int max ;
+
+        if(a>b && a>c){
+            max = a;
+            System.out.println(a);
+        }
+        else if(b>a && b>c){
+            max = b;
+            System.out.println(b);
+        }
+        else{
+            max = c;
+            System.out.println(max);
+        }
+    }
+
+}
---
+++ java DSA basic to Advance/Selflogic/Loopsk.java
@@ -0,0 +1,10 @@
+public class Loopsk {
+
+    public static void main(String[] args) {
+        int n = 1;
+        do{
+            System.out.println("HI");
+        } while (n!=1);
+    }
+
+}
