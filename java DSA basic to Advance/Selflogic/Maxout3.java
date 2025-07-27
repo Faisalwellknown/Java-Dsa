b5bba99dce1f505f54b4e13f8393d2b95b612951
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-07-28 04:06:00 +0530

    added maxout

---
+++ java DSA basic to Advance/Selflogic/Maxout3.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+public class Maxout3 {
+
+    
+    public static void main(String[] args) {
+
+        Scanner in  = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+//        int max =0;
+//        if(a>b){ max = a;
+//        }
+//        else{
+//            max=b;
+//        }
+//
+//        if(c>max){
+//            max = c;
+//        }
+
+        int max = Math.max(c, Math.max(a,b));
+
+
+
+        System.out.println(max);
+
+
+
+
+    }
+}
+
+
+
