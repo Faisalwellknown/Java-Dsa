400f39dd8af7adbb9d72306a11c4eb458a000d13
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-10-10 03:35:47 +0530

    Added area and perameter

---
+++ revisionjavaDsa/Areapar.java
@@ -0,0 +1,20 @@
+public class Areapar {
+    public static void main(String[] args) {
+        
+        int length = 10;
+        int weight = 15;
+
+        int Area = length * weight;
+
+        System.out.println("Area of rectangle : " + Area);
+
+        int Peramiter = 2*(length + weight);
+
+        System.out.println("Peramiter of rectangle : " + Peramiter);
+
+
+
+
+    }
+}
+
--- revisionjavaDsa/Hel.java
+++ revisionjavaDsa/Hel.java
@@ -1,6 +1,8 @@
 public class Hel {
     public static void main(String [] args){
         System.out.println("Assalamualaikum The world of coding ");
+        String a = "Faisal"; 
+        System.out.println("Assalamualaikum " + a);
     }
     
 }
