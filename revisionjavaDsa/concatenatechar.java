d611caf84e6939cff4b5169f870cf24b6dcfafab
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-10-12 23:59:37 +0530

    Added concate char

---
+++ revisionjavaDsa/concatenatechar.java
@@ -0,0 +1,16 @@
+public class concatenatechar {
+
+    public static void main(String arg[]){
+        char ch1 = 'F';
+        char ch2 = 'A';
+        char ch3 = 'I';
+        char ch4 = 'S';
+        char ch5 = 'A';
+        char ch6 = 'L';
+
+        String sum = ""+ ch1 + ch2 + ch3 + ch4 + ch5 + ch6 ;
+
+        System.out.println("" + sum);
+    }
+    
+}
