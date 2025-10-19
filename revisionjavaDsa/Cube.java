df2fbf566fe421c7571be90fe353f5546948b843
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-10-20 03:03:30 +0530

    Added Cube

--- revisionjavaDsa/Cube.java
+++ revisionjavaDsa/Cube.java
@@ -1,4 +1,18 @@
+
+import java.util.Scanner;
+
 public class Cube {
-    
+    public static void main(String [] args){
+        Scanner a = new Scanner (System.in);
+
+        int num = a.nextInt();
+
+        int cube = num*num*num;
+
+        System.out.println(cube);
+
+
+    }
+
     
 }
