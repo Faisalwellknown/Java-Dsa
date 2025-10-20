0b4aee422f71fde36cc45cc2a594da3de23aef1e
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-10-20 05:46:24 +0530

    Added Sum

---
+++ revisionjavaDsa/Sum.java
@@ -0,0 +1,19 @@
+public class Sum {
+
+    public static int logic(int a,int b){
+        int total = a + b;
+        return total;
+
+    }
+    
+
+    public static void main(String [] args){
+
+        int a = 6;
+        int b = 8;
+        int Sum = logic(a , b);
+
+        System.out.println(Sum);
+
+    }
+}
