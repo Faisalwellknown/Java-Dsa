602d6d022f0a5f3d8c8d1b7b5b91c73f2eb959f6
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-10-23 02:40:15 +0530

    Added Find the remainder

---
+++ revisionjavaDsa/Findriminder.java
@@ -0,0 +1,19 @@
+public class Findriminder {
+
+    public static int logic(int a , int b){
+        int z = a % b;
+
+        return z;
+        
+
+    }
+    public static void main(String []args){
+        int i = 10;
+        int j = 5;
+        int output = logic(i,j);
+        
+        System.out.println(output);
+
+    }
+    
+}
