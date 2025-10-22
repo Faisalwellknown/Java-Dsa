e43fc9198e4753ddf1ee720b3d9865236c8a4178
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-10-23 02:03:50 +0530

    Added Two Number are equal or not

---
+++ revisionjavaDsa/Twonumberequal.java
@@ -0,0 +1,29 @@
+public class Twonumberequal {
+
+
+    public static boolean  logic(int i,int k){
+        boolean a ;
+
+        if(i == k){
+            a = true; 
+        }
+        else{
+            a = false;
+        }
+
+        return a;
+
+    }
+    public static void main(String  [] args){
+
+        int c = 2;
+        int e = 2;
+        boolean u = logic(c,e);
+
+        System.out.println(u);
+
+    }
+
+
+    
+}
